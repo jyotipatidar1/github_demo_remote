f87c6b47c243c4f2820c7d328a47972c1e51d1cd
jyotipatidar1 <77095989+jyotipatidar1@users.noreply.github.com>
2022-03-04 20:02:24 +0530

    Initial comment of math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+add implimentation
+def add(x,y):
+    return x+y
+
+subtraction implimentation
+def sub(x,y):
+    pass
+
+multiply implimentation
+def mul(x,y):
+    return x*y
+
+devide implimentation
+def div(x,y):
+    if y==0:
+        return DIVIDE_BY_0
+    else:
+        return x/y
+
+def squre(x):
+    return x*x;
